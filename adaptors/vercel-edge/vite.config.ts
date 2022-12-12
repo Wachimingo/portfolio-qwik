b3aa1922da3397c9fa3cb9072649f78c36df8746
Wachimingo <alexanderguillen.adoc@gmail.com>
2022-12-12 10:12:40 -0600

    first commit

--- adaptors/vercel-edge/vite.config.ts
+++ adaptors/vercel-edge/vite.config.ts
@@ -1,20 +1,23 @@
-import { vercelEdgeAdaptor } from '@builder.io/qwik-city/adaptors/vercel-edge/vite';
-import { extendConfig } from '@builder.io/qwik-city/vite';
-import baseConfig from '../../vite.config';
+import { vercelEdgeAdaptor } from "@builder.io/qwik-city/adaptors/vercel-edge/vite";
+import { extendConfig } from "@builder.io/qwik-city/vite";
+import baseConfig from "../../vite.config";
 
 export default extendConfig(baseConfig, () => {
   return {
+    ssr: {
+      noExternal: true
+    },
     build: {
       ssr: true,
       rollupOptions: {
-        input: ['src/entry.vercel-edge.tsx', '@qwik-city-plan'],
+        input: ["src/entry.vercel-edge.tsx", "@qwik-city-plan"]
       },
-      outDir: '.vercel/output/functions/_qwik-city.func',
+      outDir: ".vercel/output/functions/_qwik-city.func"
     },
     plugins: [
       vercelEdgeAdaptor({
-        staticGenerate: true,
-      }),
-    ],
+        staticGenerate: true
+      })
+    ]
   };
 });
