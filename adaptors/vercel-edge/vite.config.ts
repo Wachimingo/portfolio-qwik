029015b1e1ccd13ad3ac0274a18dcbad2c58a1f1
Wachimingo <alexanderguillen.adoc@gmail.com>
2022-12-12 10:54:05 -0600

    first commit

--- adaptors/vercel-edge/vite.config.ts
+++ adaptors/vercel-edge/vite.config.ts
@@ -5,14 +5,14 @@ import baseConfig from "../../vite.config";
 export default extendConfig(baseConfig, () => {
   return {
     ssr: {
-      external: ["stream", "util", "events", "dns", "zlib", "timers", "crypto", "os", "url"],
+      external: ["stream", "util", "events", "dns", "zlib", "timers", "crypto", "os", "url", "fs"],
       noExternal: undefined
     },
     build: {
       ssr: true,
       rollupOptions: {
         noExternal: false,
-        external: ["stream", "util", "events", "dns", "zlib", "timers", "crypto", "os", "url"],
+        external: ["stream", "util", "events", "dns", "zlib", "timers", "crypto", "os", "url", "fs"],
         input: ["src/entry.vercel-edge.tsx", "@qwik-city-plan"]
       },
       outDir: ".vercel/output/functions/_qwik-city.func"
